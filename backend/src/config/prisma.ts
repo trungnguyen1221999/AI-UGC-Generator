11cb5033bc4c5015a6e982d5d0f2b4adf34ff409
Trung Nguyen <trungnguyen1221999@gmail.com>
2026-04-23 11:11:31 +0300

    backend: connect postgresql with neon. and using prisma orm for schema

---
+++ backend/src/config/prisma.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client";
+
+const adapter = new PrismaPg(process.env.DATABASE_URL!);
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
---
+++ backend/src/seeding/seeding.ts
@@ -0,0 +1,30 @@
+
+import 'dotenv/config';
+import { prisma } from "../config/prisma";
+
+async function main() {
+  // Create a new user with a post
+  const user = await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      id:"1",
+      image:"1"
+    },
+    
+  });
+  console.log("Created user:", user);
+
+  // Fetch all users with their posts
+ 
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
